70fbb7e106b3312ae13f2f48368155c9ceb0554a
dgeyms <dgeyms@dgeyms.com>
2024-05-03 00:05:01 +0300

    feat(product): add Build_gradle в Git end create class PoolActivity.kt

--- app/src/main/java/com.bignerdranch.bootsignalgenerator_android/MainActivity.kt
+++ app/src/main/java/com.bignerdranch.bootsignalgenerator_android/MainActivity.kt
@@ -1,22 +1,20 @@
-package com.bignerdranch.bootsignalgenerator_android
-
-
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.activity.ComponentActivity
+import com.bignerdranch.bootsignalgenerator_android.R
 
 
 class MainActivity : ComponentActivity() {
-    private lateinit var createBotButton: Button
+    private lateinit var createPoolButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        createBotButton = findViewById(R.id.createBot_button)
+        createPoolButton = findViewById(R.id.createBot_button)
 
-        createBotButton.setOnClickListener {
+        createPoolButton.setOnClickListener {
             // создаем интент для перехода на целевую активность
-            val intent = Intent(this, CreateBotActivity::class.kt)
+            val intent = Intent(this, PoolActivity::class.java)
             startActivity(intent)
         }
     }
